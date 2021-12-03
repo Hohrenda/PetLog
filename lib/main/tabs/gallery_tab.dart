0a1f7c3ea6e2cd027d075f5dc54274577b33e065
wuffeel <vladyslav.blyzniuk@nure.ua>
2021-12-03 17:21:33 +0200

    Add gallery tab

---
+++ lib/main/tabs/gallery_tab.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:pet_log/main/widgets/add_photo_button.dart';
+import 'package:pet_log/main/widgets/gallery_item.dart';
+
+class GalleryTab extends StatefulWidget {
+  const GalleryTab({Key? key}) : super(key: key);
+
+  @override
+  _GalleryTabState createState() => _GalleryTabState();
+}
+
+class _GalleryTabState extends State<GalleryTab> {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      padding: EdgeInsets.all(17.0),
+      childAspectRatio: (1 / .7),
+      crossAxisSpacing: 10.0,
+      mainAxisSpacing: 10.0,
+      children: [
+        GalleryItem(
+          onLongPress: () => {},
+          imageUrl: 'https://nikvesti.com/images/imageeditor/2021/9/16/229603/229603_1.jpg',
+        ),
+        AddPhotoButton(onTap: () => {}),
+      ],
+    );
+  }
+}
---
+++ lib/main/widgets/gallery_item.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GalleryItem extends StatelessWidget {
+  final GestureLongPressCallback? onLongPress;
+  final String? imageUrl;
+
+  const GalleryItem(
+      {Key? key, required this.onLongPress, required this.imageUrl})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onLongPress: onLongPress,
+      child: Image.network(
+        imageUrl!,
+        width: 115.0,
+        height: 115.0,
+      ),
+    );
+  }
+}
