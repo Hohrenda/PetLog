d2e38ed159e1470d23c06b6d1666da8771e1ac33
wuffeel <vladyslav.blyzniuk@nure.ua>
2021-12-11 23:44:57 +0200

    Change MedicineItem declarations with Map and GridView builder

--- lib/main/tabs/medicine_tab.dart
+++ lib/main/tabs/medicine_tab.dart
@@ -9,39 +9,35 @@ class MedicineTab extends StatefulWidget {
 }
 
 class _MedicineTabState extends State<MedicineTab> {
+  final Map<String, String> medicineItems = <String, String>{
+    'lib/assets/medicine_documents.svg': 'Documents',
+    'lib/assets/medicine_analyzes.svg': 'Analyzes',
+    'lib/assets/medicine_surgeries.svg': 'Surgeries',
+    'lib/assets/medicine_allergies.svg': 'Allergies',
+    'lib/assets/medicine_veterinarian.svg': 'Veterinarian',
+    'lib/assets/medicine_labtests.svg': 'Lab. tests',
+  };
+
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
+    var entryList = medicineItems.entries.toList();
+
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1.05,
+      ),
       physics: const NeverScrollableScrollPhysics(),
-      crossAxisCount: 2,
-      childAspectRatio: 1.05,
-      children: [
-        MedicineItem(
-          svgPictureAsset: 'lib/assets/medicine_documents.svg',
-          medicineItemText: 'Documents',
-          onTap: () => {},
-        ),
-        MedicineItem(
-            svgPictureAsset: 'lib/assets/medicine_analyzes.svg',
-            medicineItemText: 'Analyzes',
-            onTap: () => {}),
-        MedicineItem(
-            svgPictureAsset: 'lib/assets/medicine_surgeries.svg',
-            medicineItemText: 'Surgeries',
-            onTap: () => {}),
-        MedicineItem(
-            svgPictureAsset: 'lib/assets/medicine_allergies.svg',
-            medicineItemText: 'Allergies',
-            onTap: () => {}),
-        MedicineItem(
-            svgPictureAsset: 'lib/assets/medicine_veterinarian.svg',
-            medicineItemText: 'Veterinarian',
-            onTap: () => {}),
-        MedicineItem(
-            svgPictureAsset: 'lib/assets/medicine_labtests.svg',
-            medicineItemText: 'Lab. tests',
-            onTap: () => {}),
-      ],
+      itemCount: medicineItems.length,
+      itemBuilder: (context, index) {
+        return MedicineItem(
+          svgPictureAsset: entryList[index].key,
+          medicineItemText: entryList[index].value,
+          onTap: () => {
+            Navigator.of(context).pushNamed(entryList[index].value),
+          },
+        );
+      },
     );
   }
 }
