146d9db028e702813210bd89f136382045ce51b3
Hohrenda <dimatkachuk2@gmail.com>
2021-12-19 19:54:16 +0200

    Add event repository.

---
+++ lib/main/repository/event_repository.dart
@@ -0,0 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pet_log/main/models/document_model.dart';
+import 'package:pet_log/main/models/event_model.dart';
+
+class DocumentRepository {
+  final _documentCollection = 'events';
+
+  CollectionReference get collectionPath {
+    return FirebaseFirestore.instance.collection(_documentCollection);
+  }
+
+  Future<void> createEvent(EventModel eventModel) async {
+    try {
+      await collectionPath.add(eventModel.toJson());
+    } catch (error) {
+      print('Event creation error - $error');
+    }
+  }
+
+  Future<void> deleteEvent(EventModel eventModel) async {
+    try {
+      await collectionPath.doc(eventModel.id).delete();
+    } catch (error) {
+      print('Document deletion error - $error');
+    }
+  }
+
+  Future<void> updateEvent(EventModel eventModel) async {
+    try {
+      await collectionPath.doc(eventModel.id).update(eventModel.toJson());
+    } catch (error) {
+      print('Document deletion error - $error');
+    }
+  }
+
+  Future<DocumentModel?> getEvent(String documentId) async {
+    try {
+      final documentSnapshot = await collectionPath.doc(documentId).get();
+      return DocumentModel.fromDocument(documentSnapshot);
+    } catch (error) {
+      print('Document deletion error - $error');
+    }
+  }
+
+  Stream<List<EventModel>>? getAllEvents(String petId) {
+    try {
+      return collectionPath.where('petId', isEqualTo: petId).snapshots().map(
+          (querySnapshot) => querySnapshot.docs
+              .map((doc) => EventModel.fromDocument(doc))
+              .toList());
+    } catch (error) {
+      print('Error while getting documents $error');
+    }
+  }
+}
