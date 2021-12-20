7f7a65bc8e92b5e60f565ab6c42ef0ea1c1577f2
sawel24 <sawel.gnidenko@gmail.com>
2021-12-20 21:45:48 +0200

    Add event model

--- lib/main/models/event_model.dart
+++ lib/main/models/event_model.dart
@@ -1,21 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class EventModel {
-  final String id;
+  final String? id;
+  final String? petId;
   final String svgIcon;
   final String name;
   final Timestamp date;
 
   EventModel({
-    required this.id,
+    this.id,
+    required this.petId,
     required this.svgIcon,
     required this.name,
     required this.date,
   });
 
-  factory EventModel.fromJson(Map<String, dynamic> json, String id) {
+  factory EventModel.fromJson(Map<String, dynamic> json, String id,) {
     return EventModel(
         id: id,
+        petId: json['petId'],
         svgIcon: json['svgIcon'],
         name: json['name'],
         date: json['date']);
@@ -23,6 +26,7 @@ class EventModel {
 
   Map<String, dynamic> toJson() {
     return {
+      'petId':petId,
       'svgIcon': svgIcon,
       'name': name,
       'date': date,
@@ -35,6 +39,7 @@ class EventModel {
 
   EventModel copyWith({
     String? id,
+    String? petId,
     String? svgIcon,
     String? name,
     Timestamp? date,
@@ -44,11 +49,12 @@ class EventModel {
       svgIcon: svgIcon ?? this.svgIcon,
       name: name ?? this.name,
       date: date ?? this.date,
+      petId: petId ?? this.petId,
     );
   }
 
   @override
   String toString() {
-    return '[id: $id, name: $name, date: $date, svgIcon: $svgIcon]';
+    return '[id: $id, name: $name, date: $date, svgIcon: $svgIcon, petId: $petId]';
   }
 }
